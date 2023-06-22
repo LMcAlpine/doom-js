b991e4eedd3655b7f8d82a66fa179f6631fc0e44
Luke <lukedmcalpine@gmail.com>
2023-06-21 23:27:29 -0700

    draw 2d representation of vertices

--- main.js
+++ main.js
@@ -7,12 +7,19 @@ document
       return;
     }
 
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+
     const wadFileReader = new WADFileReader(file);
     const arrayBuffer = await wadFileReader.readFile();
     const wadParser = new WADParser(arrayBuffer);
     const lumpData = await wadParser.parse();
     const levelParser = new LevelParser(lumpData);
-    const levels = levelParser.parse("E1M1");
+    const levels = levelParser.parse("E1M4");
+
+    const vertices = levels.vertices;
+    const renderer = new Renderer("myCanvas");
+    renderer.drawVertices(vertices);
 
     // try {
     //   await wadParser.parse();
