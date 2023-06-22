5bee779049f81204542fa3433c062ad772a1fe3f
Luke <lukedmcalpine@gmail.com>
2023-06-21 20:35:52 -0700

    Documentation for LevelParser.js

--- levelparser.js
+++ levelparser.js
@@ -1,7 +1,21 @@
+/**
+ * The LevelParser class is used to parse level data from a set of WAD file lumps.
+ */
 class LevelParser {
+  /**
+   * Create a new level parser.
+   * @param {Array} lumps - The raw data lumps from the WAD file.
+   */
   constructor(lumps) {
     this.lumps = lumps;
   }
+
+  /**
+   * Parse a specific level from the WAD file.
+   * @param {string} levelName - The name of the level to parse.
+   * @return {Object} The parsed level data, containing vertices and linedefs.
+   * @throws {Error} If the level is not found in the WAD file.
+   */
   parse(levelName) {
     // Find the level's start index in the lumps array
     const levelStartIndex = this.lumps.findIndex(
@@ -30,11 +44,16 @@ class LevelParser {
 
     console.log(sidedefs);
 
-    // ...Parse other map components like linedefs, sidedefs, sectors, things, etc.
-
     return { vertices, linedefs };
   }
 
+  /**
+   * Parses a vertices lump from the WAD file into an array of vertices.
+   *
+   * @param {Object} verticesLump - The lump of the WAD file containing vertices data.
+   * Each vertex is represented by 4 bytes (two 16-bit signed integers for X and Y coordinates).
+   * @returns {Array} vertices - An array of vertex objects, where each vertex object has 'x' and 'y' properties.
+   */
   parseVertices(verticesLump) {
     const dataView = new DataView(verticesLump.data);
     const vertices = [];
@@ -47,6 +66,25 @@ class LevelParser {
 
     return vertices;
   }
+
+  /**
+   * Parses linedef lumps data from a WAD file
+   *
+   *   Each linedef object represents a wall segment in the map.
+   *   A linedef is a 14-byte structure containing the following fields:
+   *    startVertex: {number} - The index of the start vertex. This defines one end of the linedef.
+   *    endVertex: {number} - The index of the end vertex. This defines the other end of the linedef.
+   *    flags: {number} - Flags that determine the properties of the linedef such as blocking player, blocking projectiles etc.
+   *    specialType: {number} - The type of special effect activated by the linedef, e.g., door or lift action.
+   *    sectorTag: {number} - The tag of the sector that the linedef might activate with its special effect.
+   *    rightSidedef: {number} - The index of the right sidedef which gives details about the textures on the right side of the line.
+   *    leftSidedef: {number} - The index of the left sidedef which gives details about the textures on the left side of the line.
+   *
+   *   This function reads these fields for each linedef and returns an array of linedefs.
+   *
+   * @param {Object} linedefsLump - The linedefs lump data to parse
+   * @returns {Array} An array of linedef objects, each containing startVertex, endVertex, flags, specialType, sectorTag, rightSidedef, and leftSidedef.
+   */
 
   parseLinedefs(linedefsLump) {
     const dataView = new DataView(linedefsLump.data);
@@ -73,6 +111,24 @@ class LevelParser {
     }
     return linedefs;
   }
+
+  /**
+   * Parses sidedef lumps data from a WAD file into a more friendly format.
+   *
+   * Each sidedef in Doom contains data for wall segments.
+   * A sidedef is a 30-byte structure containing the following fields:
+   *    xOffset: {number} - The X offset (originally a 16-bit signed integer).
+   *    yOffset: {number} - The Y offset (originally a 16-bit signed integer).
+   *    upperTextureName: {string} - The name of the upper texture (originally 8 bytes).
+   *    lowerTextureName: {string} - The name of the lower texture (originally 8 bytes).
+   *    middleTextureName: {string} - The name of the middle texture (originally 8 bytes).
+   *    sector: {number} - The sector (originally a 16-bit signed integer).
+   *
+   * This function reads these fields for each sidedef and returns an array of sidedefs.
+   *
+   * @param {Object} sidedefsLump - The sidedefs lump data to parse.
+   * @returns {Array} An array of sidedef objects, each containing xOffset, yOffset, upperTextureName, lowerTextureName, middleTextureName, and sector properties.
+   */
 
   parseSidedefs(sidedefsLump) {
     const dataView = new DataView(sidedefsLump.data);
