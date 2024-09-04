4d07a45e3ec79be89272e64242695a555a96d7ba
Luke <lukedmcalpine@gmail.com>
2024-09-03 23:21:05 -0700

    Redo the structure of the file

--- linkedlist.js
+++ linkedlist.js
@@ -1,7 +1,21 @@
-class LinkedList {
-  constructor() {}
-
-  static Node = class Node {
-    constructor() {}
-  };
+class MyNode {
+  constructor(item) {
+    this.item = item;
+    this.next = null;
+  }
 }
+
+class LinkedList {
+  constructor() {
+    this.size = 0;
+    this.head = null;
+  }
+
+  add(item) {
+    let node = new MyNode(item);
+    this.head = node;
+  }
+}
+
+const myList = new LinkedList();
+myList.add(1);
