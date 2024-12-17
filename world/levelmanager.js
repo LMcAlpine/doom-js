aa6790399b151f4759c3236ce32eb39d89c17394
Luke <lukedmcalpine@gmail.com>
2024-12-16 23:24:45 -0800

    reenable visplanes

--- world/levelmanager.js
+++ world/levelmanager.js
@@ -115,116 +115,118 @@ class LevelManager {
     traverseCount = 0;
 
     // visplanes
-    // for (let i = 0; i < this.wallRenderer.visplanes.length; i++) {
-    //   let visplane = this.wallRenderer.visplanes[i];
+    for (let i = 0; i < this.wallRenderer.visplanes.length; i++) {
+      let visplane = this.wallRenderer.visplanes[i];
 
-    //   let textureWidthSky;
-    //   let textureHeightSky;
-    //   let textureDataSky;
-    //   if (visplane.textureName === "F_SKY1") {
-    //     let ep = selectedValue[1];
-    //     let skyname;
-    //     switch (Number(ep)) {
-    //       case 1:
-    //         skyname = "SKY1";
-    //         break;
-    //       case 2:
-    //         skyname = "SKY2";
-    //         break;
-    //       case 3:
-    //         skyname = "SKY3";
-    //         break;
-    //       case 4:
-    //         skyname = "SKY4";
-    //         break;
-    //     }
+      let textureWidthSky;
+      let textureHeightSky;
+      let textureDataSky;
+      if (visplane.textureName === "F_SKY1") {
+        let ep = selectedValue[1];
+        let skyname;
+        switch (Number(ep)) {
+          case 1:
+            skyname = "SKY1";
+            break;
+          case 2:
+            skyname = "SKY2";
+            break;
+          case 3:
+            skyname = "SKY3";
+            break;
+          case 4:
+            skyname = "SKY4";
+            break;
+        }
 
-    //     let r = this.wallRenderer.textureManager.texturePool.get(skyname);
-    //     textureWidthSky = r.textureWidth;
-    //     textureHeightSky = r.textureHeight;
-    //     textureDataSky = r.textureImageData;
+        let r = this.wallRenderer.textureManager.texturePool.get(skyname);
+        textureWidthSky = r.textureWidth;
+        textureHeightSky = r.textureHeight;
+        textureDataSky = r.textureImageData;
 
-    //     for (let x = visplane.minX; x <= visplane.maxX; x++) {
-    //       let topY = visplane.top[x];
-    //       let bottomY = visplane.bottom[x];
+        for (let x = visplane.minX; x <= visplane.maxX; x++) {
+          let topY = visplane.top[x];
+          let bottomY = visplane.bottom[x];
 
-    //       if (topY <= bottomY) {
-    //         let textureColumn =
-    //           (gameEngine.player.direction.angle + getXToAngle(x)) * 2.8444; // Random number. No idea. Credit to room4doom for the random number
+          if (topY <= bottomY) {
+            let textureColumn =
+              (gameEngine.player.direction.angle + getXToAngle(x)) * 2.8444; // Random number. No idea. Credit to room4doom for the random number
 
-    //         this.wallRenderer.drawColumn(
-    //           CANVASHEIGHT / 2,
-    //           topY,
-    //           bottomY,
-    //           1,
-    //           textureColumn,
-    //           textureWidthSky,
-    //           textureHeightSky,
-    //           textureDataSky,
-    //           x,
-    //           1
-    //         );
-    //       }
-    //     }
-    //     continue;
-    //   }
+            this.wallRenderer.drawColumn(
+              CANVASHEIGHT / 2,
+              topY,
+              bottomY,
+              1,
+              textureColumn,
+              textureWidthSky,
+              textureHeightSky,
+              textureDataSky,
+              x,
+              1
+            );
+          }
+        }
+        continue;
+      }
 
-    //   let flat = this.flatManager.flatPool.get(visplane.textureName);
+      let flat = this.flatManager.flatPool.get(visplane.textureName);
 
-    //   if (!flat) continue;
+      if (!flat) continue;
 
-    //   const textureWidthFlat = flat.width;
-    //   const textureHeightFlat = flat.height;
-    //   const textureData = flat.data;
+      const textureWidthFlat = flat.width;
+      const textureHeightFlat = flat.height;
+      const textureData = flat.data;
 
-    //   for (let j = visplane.minX; j <= visplane.maxX; j++) {
-    //     let topY = visplane.top[j];
-    //     let bottomY = visplane.bottom[j];
+      for (let j = visplane.minX; j <= visplane.maxX; j++) {
+        let topY = visplane.top[j];
+        let bottomY = visplane.bottom[j];
 
-    //     if (topY <= bottomY) {
-    //       // Calculate direction vectors for texture mapping
-    //       let playerDirectionX = Math.cos(
-    //         degreesToRadians(gameEngine.player.direction.angle)
-    //       );
-    //       let playerDirectionY = Math.sin(
-    //         degreesToRadians(gameEngine.player.direction.angle)
-    //       );
+        if (topY <= bottomY) {
+          // Calculate direction vectors for texture mapping
+          let playerDirectionX = Math.cos(
+            degreesToRadians(gameEngine.player.direction.angle)
+          );
+          let playerDirectionY = Math.sin(
+            degreesToRadians(gameEngine.player.direction.angle)
+          );
 
-    //       // Iterate from top to bottom in this column
-    //       for (let y = topY; y <= bottomY; y++) {
-    //         let z = (HALFWIDTH * visplane.worldFront) / (HALFHEIGHT - y);
-    //         let px = playerDirectionX * z + gameEngine.player.x;
-    //         let py = playerDirectionY * z + gameEngine.player.y;
+          // Iterate from top to bottom in this column
+          for (let y = topY; y <= bottomY; y++) {
+            let z = (HALFWIDTH * visplane.worldFront) / (HALFHEIGHT - y);
+            let px = playerDirectionX * z + gameEngine.player.x;
+            let py = playerDirectionY * z + gameEngine.player.y;
 
-    //         let leftX = -playerDirectionY * z + px;
-    //         let leftY = playerDirectionX * z + py;
-    //         let rightX = playerDirectionY * z + px;
-    //         let rightY = -playerDirectionX * z + py;
+            let leftX = -playerDirectionY * z + px;
+            let leftY = playerDirectionX * z + py;
+            let rightX = playerDirectionY * z + px;
+            let rightY = -playerDirectionX * z + py;
 
-    //         let dx = (rightX - leftX) / CANVASWIDTH;
-    //         let dy = (rightY - leftY) / CANVASWIDTH;
-    //         let tx = Math.floor(leftX + dx * j) & (textureWidthFlat - 1);
-    //         let ty = Math.floor(leftY + dy * j) & (textureHeightFlat - 1);
-    //         const texPos = (ty * textureWidthFlat + tx) * 4;
+            let dx = (rightX - leftX) / CANVASWIDTH;
+            let dy = (rightY - leftY) / CANVASWIDTH;
+            let tx = Math.floor(leftX + dx * j) & (textureWidthFlat - 1);
+            let ty = Math.floor(leftY + dy * j) & (textureHeightFlat - 1);
+            const texPos = (ty * textureWidthFlat + tx) * 4;
 
-    //         let screenPosition = y * CANVASWIDTH + j;
+            let screenPosition = y * CANVASWIDTH + j;
 
-    //         // Apply color to the screen buffer
-    //         let r = textureData[texPos];
-    //         let g = textureData[texPos + 1];
-    //         let b = textureData[texPos + 2];
-    //         let a = textureData[texPos + 3];
-    //         r = adjustColorComponent(r, visplane.lightLevel);
-    //         g = adjustColorComponent(g, visplane.lightLevel);
-    //         b = adjustColorComponent(b, visplane.lightLevel);
+            // Apply color to the screen buffer
+            let r = textureData[texPos];
+            let g = textureData[texPos + 1];
+            let b = textureData[texPos + 2];
+            let a = textureData[texPos + 3];
+            r = adjustColorComponent(r, visplane.lightLevel);
+            g = adjustColorComponent(g, visplane.lightLevel);
+            b = adjustColorComponent(b, visplane.lightLevel);
 
-    //         gameEngine.canvas.screenBuffer[screenPosition] =
-    //           (a << 24) | (b << 16) | (g << 8) | r;
-    //       }
-    //     }
-    //   }
-    // }
+            gameEngine.canvas.screenBuffer[screenPosition] =
+              (a << 24) | (b << 16) | (g << 8) | r;
+          }
+        }
+      }
+    }
 
+
+    // masked wall
     for (let i = this.wallRenderer.drawSegments.length - 1; i >= 0; i--) {
       if (this.wallRenderer.drawSegments[i].maskedTextureCol) {
         let x1 = this.wallRenderer.drawSegments[i].x1;
