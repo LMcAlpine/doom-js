5b9587cc279ae933349e9da6def936a41604aece
Luke <lukedmcalpine@gmail.com>
2025-01-11 15:44:02 -0800

    move sprite processing into SpriteManager

--- engine/main.js
+++ engine/main.js
@@ -89,7 +89,8 @@ function setupGameEngine(levels, lumpData) {
   const spriteManager = new SpriteManager(lumpData);
 
   // Process sprites
-  processSprites(spriteManager, lumpData);
+  // processSprites(spriteManager, lumpData);
+  spriteManager.processSprites();
 
   const levelManager = new LevelManager(
     levels,
@@ -121,65 +122,4 @@ function setupLevelData(levels) {
     segObjects,
     thingObjects,
   };
-}
-
-function processSprites(spriteManager, lumpData) {
-  let sprites = spriteManager.getSprites("S_START", "S_END");
-
-  startIndex = spriteManager.getIndex("S_START") + 1;
-  endIndex = spriteManager.getIndex("S_END") - 1;
-
-  let patch;
-  let spriteWidth = [];
-  let spriteOffset = [];
-  let spriteTopOffset = [];
-
-  for (let i = 0; i < sprites.length; i++) {
-    patch = gameEngine.patchNames.parsePatchHeader(sprites[i].name);
-    spriteWidth[i] = patch.width;
-    spriteOffset[i] = patch.leftOffset;
-    spriteTopOffset[i] = patch.topOffset;
-  }
-
-  installSprites(spriteNames, lumpData, startIndex, endIndex);
-}
-
-function installSprites(spriteNames, lumpData, startIndex, endIndex) {
-  let spriteName;
-  let frame;
-  let rotation;
-
-  startIndex--;
-  endIndex++;
-
-  for (let i = 0; i < spriteNames.length; i++) {
-    maxFrame = -1;
-    for (let j = startIndex + 1; j < endIndex; j++) {
-      let sprite = lumpData[j].name;
-
-      if (sprite.startsWith(spriteNames[i])) {
-        spriteName = sprite;
-        frame = sprite[4].charCodeAt(0) - "A".charCodeAt(0);
-        rotation = sprite[5] - "0";
-
-        // install sprite
-
-        installSpriteLump(j, frame, rotation, false);
-
-        if (sprite.length > 6) {
-          if (sprite[6]) {
-            frame = sprite[6].charCodeAt(0) - "A".charCodeAt(0);
-            rotation = sprite[7] - "0";
-            // install sprite
-
-            installSpriteLump(j, frame, rotation, true);
-          }
-        }
-      }
-    }
-    maxFrame++;
-
-    theSprites[i].framesCount = maxFrame;
-    theSprites[i].spriteFrames = spriteTemp;
-  }
 }
--- utils/spritemanager.js
+++ utils/spritemanager.js
@@ -13,4 +13,62 @@ class SpriteManager {
   getIndex(marker) {
     return this.lumpData.findIndex((lump) => lump.name === marker);
   }
+
+  processSprites() {
+    let sprites = this.getSprites("S_START", "S_END");
+
+    startIndex = this.getIndex("S_START") + 1;
+    endIndex = this.getIndex("S_END") - 1;
+
+    let patch;
+    let spriteWidth = [];
+    let spriteOffset = [];
+    let spriteTopOffset = [];
+
+    for (let i = 0; i < sprites.length; i++) {
+      patch = gameEngine.patchNames.parsePatchHeader(sprites[i].name);
+      spriteWidth[i] = patch.width;
+      spriteOffset[i] = patch.leftOffset;
+      spriteTopOffset[i] = patch.topOffset;
+    }
+
+    this.installSprites(spriteNames, startIndex, endIndex);
+  }
+
+  installSprites(spriteNames, startIndex, endIndex) {
+    let spriteName;
+    let frame;
+    let rotation;
+
+    startIndex--;
+    endIndex++;
+
+    for (let i = 0; i < spriteNames.length; i++) {
+      maxFrame = -1;
+      for (let j = startIndex + 1; j < endIndex; j++) {
+        let sprite = this.lumpData[j].name;
+
+        if (sprite.startsWith(spriteNames[i])) {
+          spriteName = sprite;
+          frame = sprite[4].charCodeAt(0) - "A".charCodeAt(0);
+          rotation = sprite[5] - "0";
+
+          installSpriteLump(j, frame, rotation, false);
+
+          if (sprite.length > 6) {
+            if (sprite[6]) {
+              frame = sprite[6].charCodeAt(0) - "A".charCodeAt(0);
+              rotation = sprite[7] - "0";
+
+              installSpriteLump(j, frame, rotation, true);
+            }
+          }
+        }
+      }
+      maxFrame++;
+
+      theSprites[i].framesCount = maxFrame;
+      theSprites[i].spriteFrames = spriteTemp;
+    }
+  }
 }
