10e17b35159697e7547212c71218a076d6c7b48e
Luke <lukedmcalpine@gmail.com>
2025-01-19 12:53:31 -0800

    remove unused scripts

--- vissprite.js
+++
@@ -1,9 +0,0 @@
-class Vissprite {
-  constructor(x1, x2, scale, texture, textureMid) {
-    this.x1 = x1;
-    this.x2 = x2;
-    this.scale = scale;
-    this.texture = texture;
-    this.textureMid = textureMid;
-  }
-}
